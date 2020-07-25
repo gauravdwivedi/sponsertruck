0d20078c29169a137c745ce7f8bd3dc6e5c6baec
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-07-25 21:10:42 +0530

    created redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
-import {createStore,applyMiddleware} from ''
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'react-thunk';
+import logger from 'react-logger';
+import reducer from '../reducers';
 let store;
 
 export function configureStore() {
